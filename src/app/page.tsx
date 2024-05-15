016909f2315edb2e18014b9a339be6f666b7de44
warut_r <74519790+warutr@users.noreply.github.com>
2024-05-15 20:49:46 +0700

    fix responsive and and delete folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,22 @@ import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 function card() {
   return (
-    <div className="mx-auto px-[30px]">
-      <div className="max-w-[670px] grid md:grid-cols-2 grid-rows-2 bg-white shadow-md rounded-lg">
-        <div className="flex justify-center ">
+    <div className="md:max-w-2xl max-w-sm mx-auto p-5">
+      <div className="max-w-[670px] grid md:grid-cols-2 bg-white shadow-md rounded-lg">
+        <div className="relative flex h-60 md:h-auto ">
           <img
-            className="rounded-l-lg"
+            className="object-cover rounded-l-lg hidden md:block"
             src="image-product-desktop.jpg"
-            alt="product image"
+            alt="product image desktop"
+          />
+          <img
+            className="object-cover md:hidden rounded-t-lg"
+            src="image-product-mobile.jpg"
+            alt="product image mobile"
           />
         </div>
         <div className="flex flex-col px-10 py-6 ">
-          <span className="text-dark-grayish-blue font-Montserrat uppercase tracking-[0.25rem] py-2">
+          <span className="text-dark-grayish-blue font-Montserrat uppercase tracking-[5px] py-2">
             Perfume
           </span>
           <h2 className="text-very-dark-blue text-4xl font-Fraunces py-2">
@@ -23,7 +28,7 @@ function card() {
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
-          <div className="flex flex-row py-8">
+          <div className="flex flex-row py-7">
             <p className="text-dark-cyan text-4xl font-Fraunces">$149.99</p>
             <p className="flex items-center pl-5 text-dark-grayish-blue text-lg font-normal line-through">
               $169.99
@@ -40,60 +45,3 @@ function card() {
 }
 
 export default card;
-
-/* 
-
-Perfume
-
-Gabrielle Essence Eau De Parfum
-
-A floral, solar and voluptuous interpretation composed by Olivier Polge, 
-Perfumer-Creator for the House of CHANEL.
-
-$149.99
-$169.99
-
-Add to Cart
-
-******************************************************************
-
-# Front-end Style Guide
-
-## Layout
-
-The designs were created to the following widths:
-
-- Mobile: 375px
-- Desktop: 1440px
-
-> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
-
-## Colors
-
-### Primary
-
-- Dark cyan: hsl(158, 36%, 37%)
-- Cream: hsl(30, 38%, 92%)
-
-### Neutral
-
-- Very dark blue: hsl(212, 21%, 14%)
-- Dark grayish blue: hsl(228, 12%, 48%)
-- White: hsl(0, 0%, 100%)
-
-## Typography
-
-### Body Copy
-
-- Font size (paragraph): 14px
-
-### Font
-
-- Family: [Montserrat](https://fonts.google.com/specimen/Montserrat)
-- Weights: 500, 700
-
-- Family: [Fraunces](https://fonts.google.com/specimen/Fraunces)
-- Weights: 700
-
-
-*/
